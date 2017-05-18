735f0e0a36959fa584b11c72f6ab219d865ad139
hseitsmstudent <hse.itsm.student@gmail.com>
2017-05-18 14:28:22 +0300

    Initial commit

---
+++ src/main/java/com/example/Bi3GurbanovHseApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bi3GurbanovHseApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Bi3GurbanovHseApplication.class, args);
+	}
+}
